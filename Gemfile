ab7df4d7b4b26934d50898ec6aaef3ed76af353d
Matthew Hirst <matt@devicemagic.com>
2014-01-27 23:16:28 +0200

    Using google drive sdk for spreadsheet creation.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org"
 gemspec :name => "google_drive"
 gem "highline", ">= 1.5.1", :group => :test
+gem 'google-api-client', :require => 'google/api_client'
--- lib/google_drive/oauth2_fetcher.rb
+++ lib/google_drive/oauth2_fetcher.rb
@@ -7,12 +7,18 @@ require "google_drive/basic_fetcher"
 module GoogleDrive
 
     class OAuth2Fetcher < BasicFetcher #:nodoc:
-        
+
         def initialize(auth_token, proxy)
           super(proxy)
           @auth_token = auth_token
         end
-        
+
+        def client
+          client = Google::APIClient.new
+          client.authorization.access_token = @auth_token
+          client
+        end
+
       private
 
         def auth_header(auth)
@@ -20,5 +26,5 @@ module GoogleDrive
         end
 
     end
-    
+
 end
